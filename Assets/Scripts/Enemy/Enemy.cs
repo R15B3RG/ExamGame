1bb4941923a2487b1a2c9ee57f2e011b96273449
R15B3RG <143381712+R15B3RG@users.noreply.github.com>
2025-05-15 00:57:04 +0200

    Enemy movement and animations

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,91 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float turnSpeed;
+    public float aggressionRange;
+
+    [Header("Idle data")]
+    public float idleTime;
+
+
+    [Header("Move data")]
+    public float moveSpeed;
+
+    [SerializeField] private Transform[] patrolPoints;
+    private int currentPatrolIndex;
+
+
+    public Transform player { get; private set; }
+
+    public Animator anim { get; private set; }
+    public NavMeshAgent agent { get; private set; }
+
+    public EnemyStateMachine stateMachine {  get; private set; }
+
+    protected virtual void Awake()
+    {
+        stateMachine = new EnemyStateMachine();
+
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponentInChildren<Animator>();
+
+        player = GameObject.Find("Player").GetComponent<Transform>();
+    }
+
+    protected virtual void Start()
+    {
+        InitializePatrolPoints();
+    }
+
+
+    protected virtual void Update()
+    {
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, aggressionRange);
+    }
+
+
+    public void AnimationTrigger() => stateMachine.currentState.AnimationTrigger();
+
+
+    public bool PlayerInAggressionRange() => Vector3.Distance(transform.position, player.position) < aggressionRange;
+
+    public Vector3 GetPatrolDestination()
+    {
+        Vector3 destination = patrolPoints[currentPatrolIndex].transform.position;
+
+        currentPatrolIndex++;
+
+        if(currentPatrolIndex >= patrolPoints.Length)
+            currentPatrolIndex = 0;
+
+        return destination;
+    }
+
+    private void InitializePatrolPoints()
+    {
+        foreach (Transform t in patrolPoints)
+        {
+            t.parent = null;
+        }
+    }
+
+
+    public Quaternion FaceTarget(Vector3 target)
+    {
+        Quaternion targetRotation = Quaternion.LookRotation(target - transform.position);
+
+        Vector3 currentEulerAngles = transform.rotation.eulerAngles;
+
+        float yRotation = Mathf.LerpAngle(currentEulerAngles.y, targetRotation.eulerAngles.y, turnSpeed * Time.deltaTime);
+
+        return Quaternion.Euler(currentEulerAngles.x, yRotation, currentEulerAngles.z);
+    }
+}
---
+++ Assets/Scripts/Enemy/MoveState_Melee.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveState_Melee : EnemyState
+{
+
+    private Enemy_Melee enemy;
+    private Vector3 destination;
+    public MoveState_Melee(Enemy enemyBase, EnemyStateMachine stateMachine, string animBoolName) : base(enemyBase, stateMachine, animBoolName)
+    {
+        enemy = enemyBase as Enemy_Melee;
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+
+        destination = enemy.GetPatrolDestination();
+
+        enemy.agent.SetDestination(destination);
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+
+        if (enemy.PlayerInAggressionRange())
+        {
+            stateMachine.ChangeState(enemy.recoveryState);
+            return;
+        }
+
+
+        enemy.transform.rotation = enemy.FaceTarget(GetNextPathPoint());
+
+        if(enemy.agent.remainingDistance <= enemy.agent.stoppingDistance + .05f)
+            stateMachine.ChangeState(enemy.idleState);
+    }
+
+    private Vector3 GetNextPathPoint()
+    {
+        NavMeshAgent agent = enemy.agent;
+        NavMeshPath path = agent.path;
+
+        if (path.corners.Length < 2)
+            return agent.destination;
+
+        for (int i = 0; i < path.corners.Length; i++)
+        {
+            if(Vector3.Distance(agent.transform.position, path.corners[i]) < 1)
+                return path.corners[i + 1];
+        }
+
+        return agent.destination;
+    }
+}
+    
+
